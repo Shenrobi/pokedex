ac525419c023dd4802b64067697991f0f29ba411
TreQues <terrell@flygoody.com>
2016-09-07 17:40:06 -0400

    Added background, search bar, and collection view.

---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,44 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Terrell Robinson on 9/7/16.
+//  Copyright © 2016 Terrell Robinson. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
