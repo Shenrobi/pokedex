20f28d1adc5169b303e26217c6ce0b826bdce111
TreQues <terrell@flygoody.com>
2016-09-08 17:51:21 -0400

    Added updated UI, added Alamofire, parsed PokeApi.

---
+++ pokedex/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Terrell Robinson on 9/8/16.
+//  Copyright © 2016 Terrell Robinson. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
+
+
+
+
+
+
+
+
+
+
+
+
+
--- pokedex/PokemonDetailVC.swift
+++ pokedex/PokemonDetailVC.swift
@@ -30,9 +30,43 @@ class PokemonDetailVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        nameLabel.text = pokemon.name
+        nameLabel.text = pokemon.name.capitalized
+        
+        let img = UIImage(named: "\(pokemon.pokedexId)")
+        mainImage.image = img
+        currentEvoImage.image = img
+        pokedexLabel.text = "\(pokemon.pokedexId)"
+        
+        pokemon.downloadPokemonDetail {
+            
+            // Whatever we write will only be called after the network call is complete!
+            
+            self.updateUI()
+            
+        }
 
 
+    }
+    
+    func updateUI() {
+        
+        attackLabel.text = pokemon.attack
+        defenseLabel.text = pokemon.defense
+        heightLabel.text = pokemon.height
+        weightLabel.text = pokemon.weight
+        typeLabel.text = pokemon.type
+        descriptionLabel.text = pokemon.description
+        
+        if pokemon.nextEvolutionId == "" {
+            evoLabel.text = "No Evolutions"
+            nextEvoImage.isHidden = true
+        } else {
+            nextEvoImage.isHidden = false
+            nextEvoImage.image = UIImage(named: pokemon.nextEvolutionId)
+            let str = "Next Evolution: \(pokemon.nextEvolutionName) - LVL \(pokemon.nextEvolutionLevel)"
+            evoLabel.text = str
+        }
+        
     }
 
     @IBAction func backButtonPressed(_ sender: UIButton) {
